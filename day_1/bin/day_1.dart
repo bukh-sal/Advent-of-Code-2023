a7732bcd8ad80c038db09545d3abd1cfc694498a
Salman Saeed Albukhaitan <44763729+bukh-sal@users.noreply.github.com>
2023-12-02 08:10:02 +0300

    rolling replace for step 2😊

--- day_1/bin/day_1.dart
+++ day_1/bin/day_1.dart
@@ -1,21 +1,82 @@
 import 'dart:io';
 
+String rollingReplace(String text) {
+  int textLength = text.length;
+  int startAt = 0;
+  int distanceToEnd = text.length;
+  while (startAt < textLength) {
+    bool found = false;
+
+    // 3 letter
+    if (text.length - startAt >= 3) {
+      String prefix = text.substring(0, startAt);
+      String part_1 = text.substring(startAt, startAt + 3);
+      String part_2 = text.substring(startAt + 3);
+
+      if (part_1.contains('one') |
+          part_1.contains('two') |
+          part_1.contains('six')) {
+        found = true;
+      }
+
+      part_1 = part_1.replaceAll('one', '1e');
+      part_1 = part_1.replaceAll('two', '2o');
+      part_1 = part_1.replaceAll('six', '6s');
+
+      text = prefix + part_1 + part_2;
+      textLength = text.length;
+    }
+
+    // 4 letter
+    if (text.length - startAt >= 4) {
+      String prefix = text.substring(0, startAt);
+      String part_1 = text.substring(startAt, startAt + 4);
+      String part_2 = text.substring(startAt + 4);
+
+      if (part_1.contains('four') |
+          part_1.contains('five') |
+          part_1.contains('nine')) {
+        found = true;
+      }
+
+      part_1 = part_1.replaceAll('four', '4r');
+      part_1 = part_1.replaceAll('five', '5e');
+      part_1 = part_1.replaceAll('nine', '9e');
+
+      text = prefix + part_1 + part_2;
+      textLength = text.length;
+    }
+
+    // 5 letter
+    if (text.length - startAt >= 5) {
+      String prefix = text.substring(0, startAt);
+      String part_1 = text.substring(startAt, startAt + 5);
+      String part_2 = text.substring(startAt + 5);
+
+      if (part_1.contains('three') |
+          part_1.contains('seven') |
+          part_1.contains('eight')) {
+        found = true;
+      }
+
+      part_1 = part_1.replaceAll('three', '3e');
+      part_1 = part_1.replaceAll('seven', '7n');
+      part_1 = part_1.replaceAll('eight', '8t');
+
+      text = prefix + part_1 + part_2;
+      textLength = text.length;
+    }
+
+    if (!found) {
+      startAt++;
+    }
+  }
+
+  return text;
+}
+
 List<int> parseNumbers(String text) {
-  print('before $text');
-
-  text = text.replaceAll('one', '1');
-  text = text.replaceAll('two', '2');
-  text = text.replaceAll('six', '6');
-
-  text = text.replaceAll('four', '4');
-  text = text.replaceAll('five', '5');
-  text = text.replaceAll('nine', '9');
-
-  text = text.replaceAll('three', '3');
-  text = text.replaceAll('seven', '7');
-  text = text.replaceAll('eight', '8');
-
-  print('after $text');
+  text = rollingReplace(text);
 
   List<String> textAsList = text.split('');
   List<int> results = [];
@@ -34,13 +95,13 @@ List<int> parseNumbers(String text) {
 
 void main(List<String> arguments) {
   List<String> text = File('input.txt').readAsLinesSync();
-
   int total = 0;
   for (int i = 0; i < text.length; i++) {
     List<int> entry = parseNumbers(text[i]);
     int firstNumber = entry[0];
     int lastNumber = entry[entry.length - 1];
-    total = total + firstNumber * 10 + lastNumber;
+    int sum = firstNumber * 10 + lastNumber;
+    total = total + sum;
   }
 
   print(total);
