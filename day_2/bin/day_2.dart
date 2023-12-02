a338e509aed8030eab0d9125a7bbd0c4b5502d78
bukh-sal <ssyb2014@gmail.com>
2023-12-02 21:28:46 +0300

    done 😍

--- day_2/bin/day_2.dart
+++ day_2/bin/day_2.dart
@@ -7,35 +7,79 @@ void main(List<String> arguments) {
   int maxGreen = 13;
   int maxBlue = 14;
   List<int> validGames = [];
-
-  print(games);
+  List<List<int>> maxColorValues = [];
 
   for (int i = 0; i < games.length; i++) {
-    Map<String, int> colorCount = {"red": 0, "green": 0, "blue": 0};
 
-    String game = games[i];
-    String gameId = game.split(':')[0];
+    bool         gameIsValid = true;
+    String       game        = games[i];
+    String       gameId      = game.split(':')[0];
+    String       gameDraws   = game.split(':')[1].trim();
+    List<String> draws       = gameDraws.split(';');
+    Map<String, int> maxColor = {"red": 0, "green": 0, "blue": 0};
+
     gameId = gameId.split('Game')[1];
 
-    String gameDraws = game.split(':')[1].trim();
-
-    List<String> draws = gameDraws.split(';');
     for (int a = 0; a < draws.length; a++) {
+      Map<String, int> colorCount = {"red": 0, "green": 0, "blue": 0};
       String draw = draws[a];
       List<String> cubes = draw.split(',');
       for (var element in cubes) {
-        cubes[cubes.indexOf(element)] = element.trim();
+        String cleanedName = element.trim();
+        cubes[cubes.indexOf(element)] = cleanedName;
       }
 
-      print('in draw $a there was $cubes');
+      for (var element in cubes) {
+        String color = element.split(' ')[1];
+        int value = int.parse(element.split(' ')[0]);
+        int currentValue = colorCount[color]!;
+        colorCount[color] = currentValue + value;
+
+
+        int currentMax = maxColor[color]!;
+        if (value > currentMax){
+          maxColor[color] = value;
+        }
+
+      }
+
+      if (colorCount["red"]! > maxRed) {
+        gameIsValid = false;
+      }
+      else if (colorCount["green"]! > maxGreen) {
+        gameIsValid = false;
+      }
+      else if (colorCount["blue"]! > maxBlue) {
+        gameIsValid = false;
+      }
+      else {
+      }
     }
 
-    print(draws);
+    List<int> values = [ maxColor['red']!, maxColor['green']!, maxColor['blue']! ];
+    maxColorValues.add(values);
+    
+    if (gameIsValid) {
+      validGames.add(int.parse(gameId));
+    }
 
-    print('Game ID = $gameId');
-
-    String text = '123 Gm';
-    int myNum = int.parse(text.substring(0, text.indexOf(' ')));
-    print(myNum);
   }
+  print('valid games: $validGames');
+
+  int idTotal = 0;
+  for (var element in validGames){
+    idTotal = idTotal + element;
+  }
+
+  print("result $idTotal");
+  print(maxColorValues);
+
+  double multiTotal = 0;
+  for (int game = 0; game < maxColorValues.length; game++) {
+    List<int> gameMins = maxColorValues[game];
+    multiTotal = multiTotal + (gameMins[0] * gameMins[1] * gameMins[2]);
+  }
+
+  print('power total = $multiTotal');
+
 }
